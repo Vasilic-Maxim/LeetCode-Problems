f559834bc11162b2ef27b44ba5abd9fcd8d13ad8
Maxim Vasilic <lmantenl@gmail.com>
2020-03-17 19:52:33 +0200

    443. String Compression

---
+++ problems/443. String Compression/1 - Iterate from end.py
@@ -0,0 +1,28 @@
+class Solution:
+    def compress(self, chars: list) -> int:
+        if len(chars) <= 1:
+            return len(chars)
+        
+        counter = 0
+        pointer = len(chars)-1
+        while pointer:
+            counter += 1
+
+            if chars[pointer] == chars[pointer-1]:
+                chars.pop(pointer)
+
+                if not pointer-1 and counter >= 1:
+                    chars[pointer:pointer] = list(str(counter+1))
+            else:
+                if counter > 1:
+                    chars[pointer+1:pointer+1] = list(str(counter))
+                    
+                counter = 0
+            
+            pointer -= 1
+
+        return len(chars)
+
+s = Solution()
+print(s.compress(["a","b","b","b","b","b","b","b","b","b","b","b","b"]))
+# print(s.compress(["a","a","b","b","c","c","c"]))
