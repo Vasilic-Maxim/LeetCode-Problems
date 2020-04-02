e95dca7124b413e0433e21ec727af65a906c6da1
Maxim Vasilic <lmantenl@gmail.com>
2020-04-02 21:36:27 +0300

    141. Linked List Cycle

---
+++ problems/141. Linked List Cycle/1  Two Pointers.py
@@ -0,0 +1,21 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    """
+    Time: O(n)
+    Space: O(1)
+    """
+    def hasCycle(self, head: ListNode) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+            if fast is slow:
+                return True
+
+        return False
