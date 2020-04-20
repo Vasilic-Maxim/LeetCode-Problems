7f9c7e2523013f5eda3c676bc06818869771ff77
Maxim Vasilic <lmantenl@gmail.com>
2020-04-20 14:59:38 +0300

    1008. Construct Binary Search Tree from Preorder Traversal

---
+++ problems/1008. Construct Binary Search Tree from Preorder Traversal/1 - Morris Pre-Order Traversal.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    """
+    Time: O(n)
+    Space: O(n) - if we exclude tree space (because it was expected) the space complexity will be O(1)
+    """
+    def bstFromPreorder(self, pre_order: List[int]) -> TreeNode:
+        root = current = None
+        for val in pre_order:
+            node = TreeNode(val)
+            if current is not None:
+                if node.val < current.val:
+                    node.right = current
+                    current.left = current = node
+                else:
+                    while current.right is not None and node.val > current.right.val:
+                        current.right, current = None, current.right
+
+                    node.right = current.right
+                    current.right = current = node
+            else:
+                root = current = node
+
+        while current.right is not None:
+            current.right, current = None, current.right
+
+        return root
