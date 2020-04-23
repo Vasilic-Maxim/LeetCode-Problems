93f5cc070f3b8ddc7b29b09a42818c75fa9b70ea
Maxim Vasilic <lmantenl@gmail.com>
2020-04-23 12:26:48 +0300

    283. Move Zeroes

--- problems/283. Move Zeroes/2 - Two Pointers.py
+++ problems/283. Move Zeroes/2 - Two Pointers.py
@@ -7,7 +7,7 @@ class Solution:
     def moveZeroes(self, nums: list) -> None:
         slow = fast = 0
         while fast < len(nums):
-            if nums[slow] == 0 and nums[fast] != 0:
+            if nums[fast] != 0:
                 nums[slow], nums[fast] = nums[fast], nums[slow]
 
             # wait while we find a non-zero element to
