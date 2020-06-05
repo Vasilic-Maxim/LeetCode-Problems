1ed9c38b429d3d00991cea462bd89ee94a43363d
Maxim Vasilic <lmantenl@gmail.com>
2020-06-06 00:08:11 +0300

    528. Random Pick with Weight

---
+++ problems/528. Random Pick with Weight/1 - Choices.py
@@ -0,0 +1,11 @@
+from random import random
+from typing import List
+
+
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.w = w
+
+    def pickIndex(self) -> int:
+        return random.choices(range(len(self.w)), weights=self.w)[0]
