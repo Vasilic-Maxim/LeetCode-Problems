b1d3fbfca0bd727254df9c7f235c510753f19f5d
Maxim Vasilic <lmantenl@gmail.com>
2020-07-28 00:22:47 +0300

    54. Spiral Matrix

---
+++ problems/54. Spiral Matrix/1 - Layer by Layer.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    """
+    Time: O(n)
+    Space: O(n)
+    """
+
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix:
+            return []
+
+        result = []
+        r1, r2 = 0, len(matrix) - 1
+        c1, c2 = 0, len(matrix[0]) - 1
+        while r1 <= r2 and c1 <= c2:
+            for r, c in self.iterator(r1, c1, r2, c2):
+                result.append(matrix[r][c])
+            r1, r2 = r1 + 1, r2 - 1
+            c1, c2 = c1 + 1, c2 - 1
+        return result
+
+    def iterator(self, r1: int, c1: int, r2: int, c2: int) -> tuple:
+        for c in range(c1, c2 + 1):
+            yield r1, c
+        for r in range(r1 + 1, r2 + 1):
+            yield r, c2
+        if r1 < r2 and c1 < c2:
+            for c in range(c2 - 1, c1, -1):
+                yield r2, c
+            for r in range(r2, r1, -1):
+                yield r, c1
