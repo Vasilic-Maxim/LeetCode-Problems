648f393f0c63126f39e37c40e101c46fe43dccde
Maxim Vasilic <lmantenl@gmail.com>
2020-10-10 13:03:32 +0300

    1078. Occurrences After Bigram

---
+++ problems/1078. Occurrences After Bigram/1 - Split.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    """
+    Time: O(n)
+    Space: O(w), where w is number of words
+    """
+
+    def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
+        words = text.split()
+        result = []
+        for i in range(2, len(words)):
+            if words[i - 2] == first and words[i - 1] == second:
+                result.append(words[i])
+        return result
